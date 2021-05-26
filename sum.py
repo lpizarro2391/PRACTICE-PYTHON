633d2a08276afb028a2394962df3ee5ef0b0700d
lpizarro <lpizarro2391@github.com>
2021-05-25 22:07:26 -0600

    suma

---
+++ sum.py
@@ -0,0 +1,7 @@
+valor1=0
+valor2=0
+result=0
+valor1= int(input("ingrese el valor 1: "))
+valor2 = int(input("ingrese el valor 2: "))
+result=valor1+valor2
+print('el resultado de la suma es:',result)
