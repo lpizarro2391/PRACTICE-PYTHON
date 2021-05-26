bce0750e4b5ae7c286401e39a1b3d6a3bf3d7f17
lpizarro2391 <lourdespizarro@gmail.com>
2021-05-26 10:01:35 -0600

    incremento de 2 en 2 hasta completar 15

--- incrementodedosendos.py
+++ incrementodedosendos.py
@@ -1,4 +1,9 @@
 contador = 0
-while not contador>7:
-    contador+=1
-    print(contador, end=',')
+while not contador>0:
+    if contador >= 0:
+        contador+=1
+        print(contador, end=',')
+while not contador>15:
+    if contador >= 1:
+        contador+=2
+        print(contador, end=',')
