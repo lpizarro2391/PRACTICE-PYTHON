8558a723d85a13b0f1a74b545181b5578c46f8f9
lpizarro2391 <lourdespizarro@gmail.com>
2021-05-26 17:50:01 -0600

    clasesinit1

--- Clasesinit.py
+++ Clasesinit.py
@@ -1,10 +1,15 @@
 class Figurasgeometricas:
     def __init__(self) -> None:
-        print("rectangulo")
         self.base=int(input("dame el valor de la base :"))
         self.altura=int(input("dame el valor de la altura :"))
-        self.costo=2500
-
+        self.costoterrenocuadrado=2500
+        self.costoterrenotriangular=1000
+print("rectangulo")
 rectangulo=Figurasgeometricas()
-print("el area del terreno es: ", rectangulo.base*rectangulo.altura)
-print("el costo  del lote es: ", rectangulo.base*rectangulo.altura*rectangulo.costo)
+print("triangulo")
+triangulo=Figurasgeometricas()
+
+print("el area del terreno rectangular es: ", rectangulo.base*rectangulo.altura)
+print("el costo  del lote es: ", rectangulo.base*rectangulo.altura*rectangulo.costoterrenocuadrado)
+print("el area del terreno triangular es: ", triangulo.base*triangulo.altura/2)
+print("el costo del terreno triangular es: ", (triangulo.base*triangulo.altura/2)*triangulo.costoterrenotriangular)
